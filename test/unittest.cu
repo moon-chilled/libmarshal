0499d679dee1424938b960d38b6c8b938c574340
I-Jui Sung <sung10@illinois.edu>
2012-02-09 12:35:47 -0600

    [Refactor] Improving testcase bug528

--- test/unittest.cu
+++ test/unittest.cu
@@ -76,25 +76,26 @@ void cpu_soa_asta(float *src, float *dst, int height, int width,
 };
 
 TEST_F(libmarshal_test, bug528) {
-  int h = 16;
-  int t = 4;
-  int w = 3;
-  float *src = (float*)malloc(sizeof(float)*h*w);
-  float *dst = (float*)malloc(sizeof(float)*h*w);
-  float *dst_gpu = (float*)malloc(sizeof(float)*h*w);
-  generate_vector(src, h*w);
-  cpu_soa_asta(src, dst, h, w, t);
-  float *d_dst;
-  cudaMalloc(&d_dst, sizeof(float)*h*w);
-  cudaMemcpy(d_dst, src, sizeof(float)*h*w, cudaMemcpyHostToDevice);
-  bool r = gpu_soa_asta_pttwac(d_dst, h, w, t, NULL);
-  ASSERT_EQ(false, r);
+  int h = 16*64;
+  int t = 64;
+  for (int w = 1; w < 100; w++) {
+    float *src = (float*)malloc(sizeof(float)*h*w);
+    float *dst = (float*)malloc(sizeof(float)*h*w);
+    float *dst_gpu = (float*)malloc(sizeof(float)*h*w);
+    generate_vector(src, h*w);
+    cpu_soa_asta(src, dst, h, w, t);
+    float *d_dst;
+    cudaMalloc(&d_dst, sizeof(float)*h*w);
+    cudaMemcpy(d_dst, src, sizeof(float)*h*w, cudaMemcpyHostToDevice);
+    bool r = gpu_soa_asta_pttwac(d_dst, h, w, t, NULL);
+    ASSERT_EQ(false, r);
 
-  cudaMemcpy(dst_gpu, d_dst, sizeof(float)*h*w, cudaMemcpyDeviceToHost);
-  EXPECT_EQ(0, compare_output(dst_gpu, dst, h*w));
-  free(src);
-  free(dst);
-  cudaFree(d_dst);
+    cudaMemcpy(dst_gpu, d_dst, sizeof(float)*h*w, cudaMemcpyDeviceToHost);
+    EXPECT_EQ(0, compare_output(dst_gpu, dst, h*w));
+    free(src);
+    free(dst);
+    cudaFree(d_dst);
+  }
 }
 
 TEST_F(libmarshal_test, bug525) {
